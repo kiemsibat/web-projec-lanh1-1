c2bfe9adef231eee276e00a849069e8cefe75c3b
kiemsibat <hoanglan1989vn@gmail.com>
2021-10-10 14:33:14 +0700

    update

--- client/src/index.js
+++ client/src/index.js
@@ -14,7 +14,7 @@ const App = () => {
   return (
     <ApolloProvider client={client}>
       <div className="app">
-        <h2>xin chao master</h2>
+        <h2>xin chao maste1r</h2>
         <TodoAdd />
         <TodoList />
       </div>
