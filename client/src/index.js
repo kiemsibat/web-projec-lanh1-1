827f28923b40aeb870876b9dcee1c7eeac3a5b2b
kiemsibat <hoanglan1989vn@gmail.com>
2021-10-11 23:28:20 +0700

    update

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,7 @@ import TodoList from './components/todo-list';
 import './styles.css';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
+  uri: 'http://192.168.210.134:4000/graphql',
 });
 
 const App = () => {
