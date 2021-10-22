73a5f768960ff97e562e53757253c5960e8e704a
kiemsibat <hoanglan1989vn@gmail.com>
2021-10-22 13:22:05 +0700

    update content todolist

--- client/src/index.js
+++ client/src/index.js
@@ -14,7 +14,7 @@ const App = () => {
   return (
     <ApolloProvider client={client}>
       <div className="app">
-        <h2>TodoList 1</h2>
+        <h1>TodoList</h1>
         <TodoAdd />
         <TodoList />
       </div>
